5556fe32bc7494bb868797afb307550a29fbfc67
6787020Napat <Napat.kom@student.mahidol.edu>
2026-04-19 22:34:50 +0700

    Update buildstrenght_squat.dart

--- wehealth/lib/buildstrenght_squat.dart
+++ wehealth/lib/buildstrenght_squat.dart
@@ -1,14 +1,52 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'buildstrength_pushup.dart'; // เรียกใช้ Layout จากไฟล์ pushup
+import 'buildstrength_pushup.dart'; // เรียกใช้ Layout
+import 'global_data.dart';
 
-class BuildstrengthSquat extends StatelessWidget {
+class BuildstrengthSquat extends StatefulWidget {
   const BuildstrengthSquat({super.key});
+  @override
+  State<BuildstrengthSquat> createState() => _BuildstrengthSquatState();
+}
+
+class _BuildstrengthSquatState extends State<BuildstrengthSquat> {
+  Timer? _timer;
+  int _currentSeconds = 30;
+  bool _isRunning = false;
+
+  void _handlePlayPause() {
+    if (_isRunning) {
+      _timer?.cancel();
+    } else {
+      _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        setState(() {
+          if (_currentSeconds > 0) {
+            _currentSeconds--;
+            GlobalData.addCalories(0.1); 
+          } else {
+            _timer?.cancel();
+            _isRunning = false;
+          }
+        });
+      });
+    }
+    setState(() => _isRunning = !_isRunning);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const ExerciseBaseLayout(
+    return ExerciseBaseLayout(
       title: 'Squat',
       icon: Icons.accessibility_new_rounded,
+      instruction: 'Squat low!',
+      timerDisplay: '${(_currentSeconds ~/ 60).toString().padLeft(2, '0')}:${(_currentSeconds % 60).toString().padLeft(2, '0')}',
+      isRunning: _isRunning,
+      onPlayPause: _handlePlayPause,
+      onReset: () {
+        _timer?.cancel();
+        setState(() { _currentSeconds = 30; _isRunning = false; });
+      },
+      onDone: () => Navigator.pop(context, true),
     );
   }
 }
